3e32bd676cebcc84efa3fef06bbf93a7cd0ae12d
zorislav <46168195+zorislav@users.noreply.github.com>
2019-05-06 22:13:59 +0200

    Add files via upload

---
+++ Code/l2.py
@@ -0,0 +1,13 @@
+#
+# l2.py
+#
+a = int(input("Upisi stranicu a: "))
+b = int(input("Upisi stranicu b: ")) 
+p = 0
+if a==b: 
+    p = a**2
+    print("Povrsina kvadrata iznosi: ", p)
+else:
+    p = a *b
+    print("Povrsina pravokutnika iznosi: ", p)
+
